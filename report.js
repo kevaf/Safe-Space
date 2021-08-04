f688a9fcd8dc983dd6ea9b0fd17c025d8e1f2a8c
enock <enckkipronoh@gmail.com>
2021-08-04 18:29:19 +0300

    adding the information section on reporting

--- report.js
+++ report.js
@@ -4,18 +4,41 @@
 
 $(document).ready(function(){
         $(".container").submit(function(event){
-            
-            var name1Input = $("input#name1").val();
-            var case1Input = $("#case1 option:selected").val();
            
-            $(".name1").append(name1Input);
-            $(".case1").append(case1Input);
+             
+            var name1Input = $("input#name1").val();
+
+           
+            $(".firstname").append(name1Input);
+            
          
     
             $("#report").show();
     
             event.preventDefault();
+           
         });
+       
     });
+    
+    
+// $(document).ready(function(){
+//  const name = document.getElementById('name1');
+// const lastname = document.getElementById('name3');
+// const location = document.getElementById('name2');
+// const email = document.getElementById('email1');
+// const phone = document.getElementById('phone-no');
+// const age = document.getElementById('age1');
+// const form = document.getElementById('form1');
+// const errorElement =  document.getElementById('error');
 
 
+//    let messages=[] 
+//    if(phone.length <= 9){
+//        messages.push('Phone number should be more than 9 digits')
+//    }
+//    if(messages.length > 0){
+//        e.preventDefault()
+//        errorElement.innerText = messages.join(', ')
+//    }
+// })
