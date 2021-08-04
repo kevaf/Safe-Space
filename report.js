4f7c7d6d2a7fd293ac68f14c1b85cd2d9c4b611a
enock <enckkipronoh@gmail.com>
2021-08-04 13:33:53 +0300

    making to report to the user done

--- report.js
+++ report.js
@@ -3,16 +3,14 @@
 // business logic
 
 $(document).ready(function(){
-        $("#form").submit(function(event){
+        $(".container").submit(function(event){
+            
             var name1Input = $("input#name1").val();
-            var cae1Input = $("input#case1").val();
+            var case1Input = $("#case1 option:selected").val();
            
             $(".name1").append(name1Input);
             $(".case1").append(case1Input);
-            // $(".animal").append(animalInput);
-            // $(".exclamation").append(exclamationInput);
-            // $(".verb").append(verbInput);
-            // $(".noun").append(nounInput);
+         
     
             $("#report").show();
     
@@ -21,22 +19,3 @@ $(document).ready(function(){
     });
 
 
-// $(document).ready(function() {
-    
-//     $(".form-group #form").submit(function(event) {
-//       var blanks = ["name1", "case1"];
-
-//       form.forEach(function(forms) {
-      
-//         var userInput = $("input." + forms).val();
-//         // below are important tools of trade for debugging
-//         // debugger
-//         // console.log(userInput);
-//         $("." + forms).text(userInput);
-//       });
-
-//       $("#report").show();
-
-//       event.preventDefault();
-//     });
-//   });
